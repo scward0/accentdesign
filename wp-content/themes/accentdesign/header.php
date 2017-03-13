0016ca708093f10230b94e2ea5ef57d7bba2f4ff
Sean Ward <scward0@gmail.com>
2017-03-13 11:06:05 -0600

    hard links to stylesheets

--- wp-content/themes/accentdesign/header.php
+++ wp-content/themes/accentdesign/header.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php wp_title(); ?></title>
+    <link rel="stylesheet" href="http://198.58.102.147/accentdesign/wp-content/themes/accentdesign/css/foundation.css">
+    <link rel="stylesheet" href="http://198.58.102.147/accentdesign/wp-content/themes/accentdesign/style.css">
     <?php wp_head(); ?>
   </head>
 
